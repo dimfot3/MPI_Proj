5b7efc627f2e7ad67ceb8591511f61bab56d5aca
dimfot3 <fotiou.dimitris3@gmail.com>
2021-12-17 23:18:27 +0200

    random added

--- src/sprogram.c
+++ src/sprogram.c
@@ -98,7 +98,7 @@ int main(int argc, char** argv) {
     float *pivot_point = (float*)malloc(sizeof(float)*points.dim);
     if(world_rank == 0)
     {
-        pivot = 6569;
+        pivot = rand()%points.num;
         memcpy(pivot_point, points.points[pivot], sizeof(float)*points.dim);
         printf("Pivot is %d\n", pivot);       
     }
