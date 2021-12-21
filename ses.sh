683d26238301af747a32e24580c215ea5d801fd4
Dimitrios <fdimitri@gapps.auth.gr>
2021-12-22 01:03:55 +0200

    added handled for file and ses script formulated

--- ses.sh
+++ ses.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in 4
+for i in 2 4
 do 
-	for file in $(ls data/$1)
+	for file in $(ls $1)
 	do
-		mpirun -n $i ./sprogram ../data/$1/$file
+		mpirun -n $i ./sprogram $1/$file
 	done
 done
 
