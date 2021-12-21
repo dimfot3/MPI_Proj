af7513315bbb595f7c1cd4daa86628fac782a86a
dimfot3 <fotiou.dimitris3@gmail.com>
2021-12-21 19:22:44 +0200

    some scripts used to plot

--- scripts/plots.py
+++ scripts/plots.py
@@ -2,34 +2,30 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dt_path = {"Uniform": "./data/uni.txt","Normal": "./data/norm.txt","Expodential": "./data/exp.txt", "Mnist":"./data/mnist.txt" }
 
+#num plots
+dt_path = {"Uniform": "./res/uni.txt","Normal": "./res/norm.txt","Expodential": "./res/exp.txt", "Mnist":"./res/mnist.txt" }
+f,ax = plt.subplots(1, 1, figsize=(10,7))
 for i in dt_path.keys():
-    f,ax = plt.subplots(1, 1, figsize=(10,7))
     path = dt_path[i]
     df = pd.read_csv (path, names=['num_points', 'dim', 'proc', 'time'])
-    best_dim = df['dim'].mode()[0]
-    temp = df.loc[df.proc==2].loc[df.dim==best_dim].sort_values(by=['num_points'])
-    ax.plot(temp.num_points, temp.time*1000, label="2 Processes")
-    temp = df.loc[df.proc==4].loc[df.dim==best_dim].sort_values(by=['num_points'])
-    ax.plot(temp.num_points, temp.time*1000, label="4 Processes")
+    temp = df.loc[(df.dim==784) & (df.time<0.8)]
+    ax.scatter(temp.num_points, temp.time*1000, label=f"{i}")
     ax.set_xlabel("Number of Points", fontsize=18)
     ax.set_ylabel("Time(ms)", fontsize=18)
-    ax.set_title(f"{i} distribution: Effect of Number of points (with dimension {best_dim})", fontsize=20)
-    ax.legend(fontsize=18)
-    f.savefig(f'{i}_num_effect.png', dpi=150, bbox_inches='tight')
+    ax.set_title(f"Effect of Number of points (with dimension 784) in different distributions", fontsize=20)
+ax.legend(fontsize=18)
+f.savefig(f'num_effect.png', dpi=150, bbox_inches='tight')
 
-    f, ax = plt.subplots(1, 1, figsize=(10, 7))
-    best_num = df['num_points'].mode()[0]
-    temp = df.loc[df.num_points==best_num].loc[df.proc==2].sort_values(by=['dim'])
-    ax.plot(temp.dim, temp.time*1000, label="2 Processes")
-    temp = df.loc[df.num_points==best_num].loc[df.proc==4].sort_values(by=['dim'])
-    ax.plot(temp.dim, temp.time*1000, label="4 Processes")
-    ax.set_xlabel("Number of dimension", fontsize=18)
+#dim plot
+f,ax = plt.subplots(1, 1, figsize=(10,7))
+for i in dt_path.keys():
+    path = dt_path[i]
+    df = pd.read_csv (path, names=['num_points', 'dim', 'proc', 'time'])
+    temp = df.loc[(df.num_points==32000) & (df.time<0.3)]
+    ax.scatter(temp.dim, temp.time*1000, label=f"{i}")
+    ax.set_xlabel("Dimension of Points", fontsize=18)
     ax.set_ylabel("Time(ms)", fontsize=18)
-    ax.set_title(f"{i} distribution: Effect of Dimension of points (with number of points {best_num})", fontsize=20)
-    ax.legend(fontsize=18)
-    f.savefig(f'{i}_dim_effect.png', dpi=150, bbox_inches='tight')
-
-
-
+    ax.set_title(f"Effect of Dimenesion of points (with number of points 32000) in different distributions", fontsize=20)
+ax.legend(fontsize=18)
+f.savefig(f'dim_effect.png', dpi=150, bbox_inches='tight')
--- scripts/variance.py
+++ scripts/variance.py
@@ -2,16 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-df = pd.read_csv("hist_thr.txt", names=['num_points', 'dim', 'proc', 'time'])
-plt.hist(df['time'], bins=20)
-plt.title("Distribution of execution time for 16 processes and same pivot and dataset", fontsize=20)
+
+df = pd.read_csv("res/hist_thr.txt", names=['num_points', 'dim', 'proc', 'time'])
+plt.hist(df['time']*1000, bins=20)
+plt.title("Distribution of execution time for 16 processes for same pivot and dataset", fontsize=20)
 plt.xlabel("Time(ms)", fontsize=18)
 plt.ylabel("Number of occurances", fontsize=18)
 plt.show()
-"""
 
-df = pd.read_csv("hist_vs.txt", names=['num_points', 'dim', 'proc', 'time'])
+
+"""
+df = pd.read_csv("./res/hist_vs.txt", names=['num_points', 'dim', 'proc', 'time'])
 val = df.proc.unique()
 counter = 0
 for i in val:
@@ -20,5 +21,6 @@ for i in val:
 plt.xticks(range(0, counter),val)
 plt.title("Mean execution time in different number of processes", fontsize=20)
 plt.xlabel("Number of processes", fontsize=18)
-plt.ylabel("Execution time(ms)", fontsize=18)
+plt.ylabel("Execution time(s)", fontsize=18)
 plt.show()
+"""
